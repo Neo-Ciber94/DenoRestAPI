72d772a9292d7138fa17d25939bd1afc6b652f2b
Freddy L <neo-ciber_94@hotmail.com>
2022-05-20 00:04:28 -0400

    Updated emailer worker

--- src/worker/email-sender.worker.ts
+++ src/worker/email-sender.worker.ts
@@ -4,6 +4,7 @@ import { RedisMessageSubscriber } from "../services/redis-pubsub.service.ts";
 import { ErrorReport } from "../types/error-report.ts";
 import { SmtpEmailService } from "../services/smtp-email.service.ts";
 import { Config } from "../config/mod.ts";
+import logger from "../config/logger.ts";
 
 const worker = self as unknown as Worker;
 const redis = await connectToRedis();
@@ -20,11 +21,14 @@ const emailSender = new SmtpEmailService();
 
 subscriber.onReceive(async (message) => {
   const { method, status, url } = message;
+  const to = "boniyeh816@dufeed.com";
 
   await emailSender.send({
+    to,
     from: Config.EMAIL_USERNAME,
-    to: "boniyeh816@dufeed.com",
     subject: `Error ${status} ocurred`,
     content: `An error ${status} ocurred on ${method}: ${url}`,
   });
+
+  logger.info(`Send email to ${to}`);
 });
