59fc48fb5b1c72ffb1c17957df07e8db9dca9137
Freddy L <neo-ciber_94@hotmail.com>
2022-05-24 22:30:49 -0400

    minor changes

--- src/ssr/utils.ts
+++ src/ssr/utils.ts
@@ -2,10 +2,10 @@
  * Checks whether the environment is running in the server.
  * @returns `true` if the current environment is a server-side rendering environment.
  */
-export const isServer = () => typeof window.Deno !== "undefined";
+export const isServer = () => typeof self.Deno !== "undefined";
 
 /**
  * Checks whether the environment is running in the browser.
  * @returns `true` if the current environment is a browser.
  */
-export const isBrowser = () => typeof window.Deno === "undefined";
+export const isBrowser = () => typeof self.Deno === "undefined";
